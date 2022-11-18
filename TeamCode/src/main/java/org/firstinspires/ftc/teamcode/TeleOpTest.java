bb1726a32d06412190b871f07aab08bbe4e33413
FlourishAndBots <teamflourishandbots@gmail.com>
2022-11-18 17:09:37 -0600

    11/18 elevator encoders updated code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -36,6 +36,7 @@ public class TeleOpTest extends LinearOpMode {
         Swing = hardwareMap.get(CRServo.class, "Swing");
 
         robot.RightElevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.LeftElevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
         // Send telemetry message to signify robot waiting;
@@ -43,31 +44,23 @@ public class TeleOpTest extends LinearOpMode {
         telemetry.update();
 
         robot.RightElevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
         robot.RightElevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.RightElevator.setTargetPosition(50);
 
         robot.LeftElevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
         robot.LeftElevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.LeftElevator.setTargetPosition(50);
 
         int leftPos = robot.LeftElevator.getCurrentPosition();
         int rightPos = robot.RightElevator.getCurrentPosition();
+
+
+
 
         while (opModeIsActive()) {
             //when play is pressed on ds this is what shows up
             telemetry.addData("Mode:", "running");
             telemetry.update();
 
-            while (robot.LeftElevator.isBusy() || robot.RightElevator.isBusy()){
-                robot.LeftElevator.getCurrentPosition();
-                robot.RightElevator.getCurrentPosition();
 
-                telemetry.addData("left position", leftPos);
-                telemetry.addData("right position", rightPos);
-                telemetry.update();
-            }
             //defines values for the gamepad sticks and buttons
             float left = gamepad1.left_stick_y;
             float right = gamepad1.right_stick_y;
@@ -79,36 +72,58 @@ public class TeleOpTest extends LinearOpMode {
 
 
             //driving
-            if (gamepad1.right_stick_y < -0.1) {
-                robot.setMotorPowers(1, 0, 1, 0);
+            if (gamepad1.right_stick_y < -0.2 || gamepad1.left_stick_y < -0.2 || gamepad1.left_stick_y > 0.2 || gamepad1.right_stick_y > 0.2) {
+                robot.setMotorPowers(left,left,right,right);
             }
-
-            if (gamepad1.right_stick_y > 0.1) {
-                robot.setMotorPowers(-1, 0, -1, 0);
+            //stop
+            else if (gamepad1.right_stick_y < 0.2 || gamepad1.left_stick_y < 0.2 || gamepad1.left_stick_y > -0.2 || gamepad1.right_stick_y > -0.2) {
+                robot.setMotorPowers(0,0,0,0);
             }
-
-            if (gamepad1.left_stick_y < -0.1) {
-                robot.setMotorPowers(0, 1, 0, 1);
+            //right strafe
+            if (gamepad1.right_trigger > 0.1) {
+                robot.setMotorPowers(-1,1,1,-1);
             }
+            //left strafe
+            else if (gamepad1.left_trigger > 0.1) {
+                robot.setMotorPowers(1,-1,-1,1);
 
-            if (gamepad1.left_stick_y > 0.1) {
-                robot.setMotorPowers(0, -1, 0, -1);
-            } else {
-                robot.setMotorPowers(0, 0, 0, 0);
             }
 
             //elevator control
-            if (gamepad2.left_stick_y > 0.1) {
-
-                robot.setElevatorPower(1, 1);
-
+            if (gamepad2.left_stick_y < -0.2) {
+                robot.RightElevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                robot.LeftElevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                robot.setElevatorPower(1,1);}
+            else if (gamepad2.left_stick_y > 0.2) {
+                robot.RightElevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                robot.LeftElevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                robot.setElevatorPower(-1,-1);}
+            else if (gamepad2.a || gamepad2.b || gamepad2.y) {
+                robot.RightElevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                robot.LeftElevator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                robot.RightElevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                robot.LeftElevator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                if (gamepad2.a) {
+                    robot.RightElevator.setTargetPosition(200);
+                    robot.LeftElevator.setTargetPosition(200); }
+                else if (gamepad2.b) {
+                    robot.RightElevator.setTargetPosition(400);
+                    robot.LeftElevator.setTargetPosition(400); }
+                else if (gamepad2.y) {
+                    robot.RightElevator.setTargetPosition(600);
+                    robot.LeftElevator.setTargetPosition(600); }
+                robot.RightElevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.LeftElevator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.setElevatorPower(1,1);
+                while (opModeIsActive() && robot.LeftElevator.isBusy()){
+                    telemetry.addData("encoder left:", robot.LeftElevator.getCurrentPosition());
+                    telemetry.addData("encoder right:", robot.RightElevator.getCurrentPosition());
+                    telemetry.update();
+                    idle();
+                }
+                robot.setElevatorPower(0,0);
             }
 
-            if (gamepad2.left_stick_y < -0.1) {
-                robot.setElevatorPower(0.2, 0.2);
-            } else {
-                robot.setElevatorPower(0, 0);
-            }
 
             //claw
 
